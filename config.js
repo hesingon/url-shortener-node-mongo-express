872211999f42ea414a074d12f6b43bb6e8a666cb
He Xing Darius <E0007888@u.nus.edu>
2017-06-27 14:29:51 +0800

    change webhost variable

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var config = {};
 config.db = {};
 // the URL shortening host - shortened URLs will be this + base58 ID
 // i.e.: http://localhost:3000/3Ys
-config.webhost = 'http://localhost:3000/';
+config.webhost = 'https://d-urlshortener.herokuapp.com/';
 
 // your MongoDB host and database name
 config.db.host = 'localhost';
